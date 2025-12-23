c45ca54f8f2b763f7ec7ab1367893a988c2c2e45
sudhiksha vanapalli <siddukrishna99@gmail.com>
2025-12-23 06:47:24 +0530

    Add loop to print squares of integers
    Implement a program to print squares of non-negative integers up to n.

---
+++ loops.py
@@ -0,0 +1,8 @@
+"""Task
+The provided code stub reads an integer, , from STDIN. For all non-negative integers i less than n, print i square.
+code:"""
+
+if __name__ == '__main__':
+    n=int(input())
+    for i in range(n):
+        print(i**2)
